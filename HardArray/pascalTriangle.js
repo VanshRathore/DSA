92cb918036d80b20944c2eb7762bbb386ca4105a
Vansh Rathore <vanshr083@gmail.com>
2025-07-02 20:21:20 +0530

    optimal way of pascal problem 1

---
+++ HardArray/pascalTriangle.js
@@ -0,0 +1,20 @@
+// 1)problem 
+
+function nCr(n , r) {
+    res = 1 ;
+    for (let i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res/(i+1);
+    }
+    return res;
+}
+    function PascalTriangle1(r, c) {
+    const element = nCr(r - 1, c - 1);
+    return element;
+    }
+
+
+const r = 1; 
+const c = 1;
+const element = PascalTriangle1(r, c);
+console.log(`The element at position (${r},${c}) is: ${element}`);
